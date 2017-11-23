cec1a1a5989a07c2f0241c8490fe1aeb48f66dd9
Jasper Cooper <jaspercooper@gmail.com>
2017-11-23 09:15:19 -0500

    Added perm function in

--- R/inspecting_dags.R
+++ R/inspecting_dags.R
@@ -147,6 +147,25 @@ get_observable_data <- function(variables){
 	return(observable_data)
 }
 
+#' Produces the possible permutations of a set of variables
+#'
+#' @param v A vector of integers indicating the number of values each variable takes on. E.g., a binary variable is represented by 2.
+#'
+#' @export
+#'
+#' @return A matrix of permutations
+#'
+#' @examples
+#'
+#' perm(c(2,2,2))
+#'
+perm <- function (v) {
+	sapply(1:length(v), function(x) {
+		rep(rep(1:v[x], each = prod(v[x:length(v)])/v[x]), length.out = prod(v))
+	}) - 1
+}
+
+
 
 
 
