bf2a38d15ddf0d61e43785eef445017f852f6190
gerasy1987 <gs2716@columbia.edu>
2017-12-11 11:48:26 -0500

    implement chains and get_terminal_vars / ambiguity matrices need rework

--- R/inspecting_dags.R
+++ R/inspecting_dags.R
@@ -1,3 +1,56 @@
+
+#' Create list by endogenous variable containing all chains terminating at this endogenous variable
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param dag DESCRIPTION.
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' \dontrun{
+#' test_dag <-
+#' gbiqq::make_dag(add_edges(parent = "X",children = c("K", "Y1")),
+#' 								add_edges(parent = "K", children = c("Y1","Y2")),
+#' 								add_edges(parent = "Z", children = c("K")))
+#'
+#' get_chains(test_dag)
+#' }
+#'
+#' @export
+get_chains <- function(dag) {
+
+	n <- 0
+	chains_out <- setNames(object = dag, nm = c("parent", paste0("children", n)))
+
+	repeat {
+		n <- n + 1
+		temp_dag <- setNames(object = dag, nm = c("parent",paste0("children", n)))
+		chains_out <-
+			suppressWarnings(
+				base::merge(temp_dag, chains_out,
+										by.x = paste0("children", n), by.y = "parent", all.y = TRUE, suffixes = c("", n))
+			)
+
+		parents <- chains_out$parent
+		if (all(is.na(parents))) {
+			chains_out <- chains_out[,-which(names(chains_out) == "parent")]
+			break
+		}
+	}
+
+	chains_out <- split(chains_out, f = chains_out$children0)
+
+	chains_out <-
+		lapply(chains_out, FUN = function(df) {
+			lapply(split(df, seq(nrow(df))), FUN = function(row) {unname(row[!is.na(row)])})
+		})
+
+	return(chains_out)
+}
+
+
+
+
 #' Get list of parent variables in a dag
 #'
 #' @param dag A dag created by make_dag()
@@ -17,29 +70,59 @@
 #' get_parents(dag)
 #' }
 #'
-get_parents <- function(dag){
-	if(nrow(dag)>1){
-		dag <- as.data.frame(apply(dag,2,as.character))
-	}
-	variables <- as.character(
-		unique(
-			unlist(dag)
-		)
-	)
-	parents <- lapply(
-		X = variables,
-		FUN = function(variable) as.character(dag$parent[dag$child == variable])
-	)
-	names(parents) <- variables
 
-	# Re-order by seniority
-	n_parents <- sapply(parents,length)
+get_parents <- function(dag) {
 
-	variables <- variables[order(n_parents)]
-	parents <- parents[variables]
+	chains <- get_chains(dag)
+	exogenous <- get_exogenous_vars(dag)
 
-	return(parents)
+	endogenous_parents <-
+		sapply(X = names(chains),
+					 FUN = function(endog_name) {
+
+					 	parents <- unlist(chains[[endog_name]])
+
+					 	setdiff(x = unique(c(base::intersect(parents, exogenous),
+					 											 base::setdiff(parents, exogenous))),
+					 					y = endog_name)
+					 })
+
+	exogenous_parents <-
+		sapply(exogenous,
+					 FUN = function(vars) { character(0) },
+					 simplify = FALSE)
+
+	return(append(x = exogenous_parents,
+								values = endogenous_parents))
+
 }
+
+## OLD VERSION
+# get_parents <- function(dag){
+# 	if(nrow(dag)>1){
+# 		dag <- as.data.frame(apply(dag,2,as.character))
+# 	}
+# 	variables <- as.character(
+# 		unique(
+# 			unlist(dag)
+# 		)
+# 	)
+# 	parents <- lapply(
+# 		X = variables,
+# 		FUN = function(variable) as.character(dag$parent[dag$child == variable])
+# 	)
+# 	names(parents) <- variables
+#
+# 	# Re-order by seniority
+# 	n_parents <- sapply(parents,length)
+#
+# 	variables <- variables[order(n_parents)]
+# 	parents <- parents[variables]
+#
+# 	return(parents)
+# }
+
+
 
 #' Get list of types for variables in a DAG
 #'
@@ -59,18 +142,10 @@ get_parents <- function(dag){
 #' get_types(dag)
 #' }
 #'
-get_types <- function(dag){
-
-	parents <- get_parents(dag)
-
-	n_variables <- length(parents)
-
-	variable_names <- names(parents)
-
-	n_parents <- sapply(parents,length)
+get_types <- function(dag) {
 
 	types <- lapply(
-		X = n_parents,
+		X = lapply(get_parents(test_dag), length),
 		FUN = function(parent_n){
 			type_mat <- perm(rep(2,2^parent_n))
 			if(parent_n == 0){
@@ -83,8 +158,37 @@ get_types <- function(dag){
 			return(type_mat)
 		}
 	)
+
 	return(types)
 }
+
+## OLD VERSION
+# get_types <- function(dag){
+#
+# 	parents <- get_parents(dag)
+#
+# 	n_variables <- length(parents)
+#
+# 	variable_names <- names(parents)
+#
+# 	n_parents <- sapply(parents,length)
+#
+# 	types <- lapply(
+# 		X = n_parents,
+# 		FUN = function(parent_n){
+# 			type_mat <- perm(rep(2,2^parent_n))
+# 			if(parent_n == 0){
+# 				labels <- NULL
+# 			} else {
+# 				input_mat <- perm(rep(2,parent_n))
+# 				labels <- apply(input_mat,1,paste,collapse = "")
+# 			}
+# 			colnames(type_mat) <- labels
+# 			return(type_mat)
+# 		}
+# 	)
+# 	return(types)
+# }
 
 #' Get exogenous variables in a DAG
 #'
@@ -94,8 +198,10 @@ get_types <- function(dag){
 #'
 #' @return A vector of exogenous variables
 get_exogenous_vars <- function(dag){
-	parents <- get_parents(dag)
-	names(parents)[sapply(parents,length) == 0]
+	parents <- unique(dag$parent)
+	return(
+		as.character(parents[!(parents %in% dag$children)])
+	)
 }
 
 #' Get endogenous variables in a DAG
@@ -106,8 +212,28 @@ get_exogenous_vars <- function(dag){
 #'
 #' @return A vector of endogenous variables
 get_endogenous_vars <- function(dag){
-	parents <- get_parents(dag)
-	names(parents)[sapply(parents,length) > 0]
+	return(
+		as.character(unique(dag$children))
+	)
+}
+
+
+
+#' Get endogenous variables in a DAG which are also terminal
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param dag DESCRIPTION.
+#'
+#' @return RETURN_DESCRIPTION
+#'
+#' @export
+#'
+get_terminal_vars <- function(dag){
+	children <- unique(dag$children)
+	return(
+		as.character(children[!(children %in% dag$parent)])
+	)
 }
 
 
@@ -119,7 +245,7 @@ get_endogenous_vars <- function(dag){
 #'
 #' @return A vector of variable names
 get_variables <- function(dag){
-	names(get_parents(dag))
+	return(names(get_parents(dag)))
 }
 
 
