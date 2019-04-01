2b711b45f1f4ab434a7a8531417b2e5043d1350d
Lily Medina <lilymiru@gmail.com>
2019-04-01 15:32:07 +0200

    gbiqq() and make_biqq_data

--- R/prepping_stan_using_dags.R
+++ R/prepping_stan_using_dags.R
@@ -306,6 +306,137 @@ get_indicator_matrix  <- function(dag){
 
 
 
+#' Prepare data for stan
+#'
+#' Create a list containing the data to be passed to stan
+#'
+#' @param dag A dag as created by \code{make_dag}
+#' @param data A data frame with observations
+#' @param lambdas_prior A vector containg priors for lambda
+#' @return a list
+#' @keywords internal
+#'
+make_gbiqq_data <- function(dag, data, lambdas_prior ){
+
+	P <- get_indicator_matrix(dag )
+	inverted_P <- 1-P
+	A <- get_ambiguities_matrix(dag )
+	likelihood_helpers <- get_likelihood_helpers(dag)
+	A_w <- likelihood_helpers$A_w
+	possible_data <-   get_max_possible_data(dag )
+	P.lambdas <- P*lambdas_prior   +	1 - P
+	prob_of_types <- apply(P.lambdas, 2, prod)
+	w <- A %*% prob_of_types # Prob of (fundamental) data realization
+	w_full <- A_w %*% w
+	n_types_each <- sapply(gbiqq:::get_nodal_types(dag), length)
+	n_vars <- length(get_variables(dag ))
+	l_ends <- cumsum(n_types_each)
+	l_starts <- c(1, l_ends[1:(n_vars-1)] + 1)
+
+	stan_data <- 	list(n_vars = n_vars,
+										 n_nodal_types = nrow(P),
+										 n_types = ncol(P),
+										 n_types_each = n_types_each,
+										 n_data = nrow( possible_data),
+										 n_events = nrow(A_w),
+										 n_strategies = likelihood_helpers$n_strategies,
+										 lambdas_prior = lambdas_prior,
+										 l_starts = l_starts,
+										 l_ends = l_ends,
+										 strategie_starts = likelihood_helpers$w_starts,
+										 strategie_ends = likelihood_helpers$w_ends,
+										 P = P,
+										 inverted_P = inverted_P,
+										 A = A,
+										 A_w= A_w,
+										 Y =get_data_events(data = dat, dag = dag )$data_events$count)
+}
+
+#' Fit stan model
+#'
+#' @param dag A dag as created by \code{make_dag}
+#' @param data A data frame with observations
+#' @param lambdas_prior A vector containg priors for lambda
+#' @importFrom devtools is.package as.package
+#' @export
+#'
+gbiqq <- function(dag, lambda_priors,  data, ...) {
 
 
+simpler <-"
+data {
+
+int<lower=1> n_vars;
+int<lower=1> n_nodal_types;
+int<lower=1> n_types;
+int<lower=1> n_types_each[n_vars];
+int<lower=1> n_data;
+int<lower=1> n_events;
+int<lower=1> n_strategies;
+
+vector<lower=0>[n_nodal_types] lambdas_prior;
+int<lower=1> l_starts[n_vars];
+int<lower=1> l_ends[n_vars];
+int<lower=1> strategie_starts[n_strategies];
+int<lower=1> strategie_ends[n_strategies];
+
+vector[n_types] P[n_nodal_types] ;
+vector[n_types] inverted_P[n_nodal_types] ;
+matrix<lower=0,upper=1>[n_data, n_types] A;
+matrix<lower=0,upper=1>[n_events,n_data] A_w;
+int<lower=0,upper=2147483647> Y[n_events]; // incorrect dimensionality, only for the tryout
+
+}
+
+parameters {
+vector<lower=0>[n_nodal_types - n_vars] gamma;
+}
+
+transformed parameters {
+vector<lower=0>[n_nodal_types] lambdas;
+vector<lower=1>[ n_vars] sum_gammas;
+for (i in 1:n_vars) {
+
+sum_gammas[i] =
+1 + sum(gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]);
+
+lambdas[l_starts[i]:l_ends[i]] =
+append_row(1, gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]) / sum_gammas[i];
+
+}
+}
+
+model {
+vector[n_data] w;
+vector[n_events] w_full;
+vector[n_types] prob_of_types;
+vector[n_nodal_types] P_lambdas[n_types];
+
+for (i in 1:n_types) {
+for (j in 1:n_nodal_types) {
+P_lambdas[i, j] = P[j, i] .* lambdas[j] + inverted_P[j, i];
+}
+prob_of_types[i] = prod(P_lambdas[i]);
+}
+
+w = A * prob_of_types;
+w_full = A_w * w;
+
+
+target += gamma_lpdf(lambdas  | lambdas_prior, 1);
+for (i in 1:n_vars) {
+target += -n_types_each[i] * log(sum_gammas[i]);
+}
+
+for (i in 1:n_strategies) {
+target += multinomial_lpmf(
+Y[strategie_starts[i]:strategie_ends[i]] | w_full[strategie_starts[i]:strategie_ends[i]]);
+}
+}
+"
+
+	stan_data <- make_gbiqq_data(dag, data, lambda_priors)
+	rstan::stan(model_code = simpler ,
+							data = stan_data,  ...)
+}
 
--- other/simpler.stan
+++ other/simpler.stan
@@ -1,70 +1,69 @@
 data {
 
-	int<lower=1> N_vars;
-	int<lower=1> N_nodal_types;
-	int<lower=1> N_types;
-	int<lower=1> N_types_each[N_vars];
-	int<lower=1> N_data;
-	int<lower=1> N_events;
-	int<lower=1> N_strategies;
+	int<lower=1> n_vars;
+	int<lower=1> n_nodal_types;
+	int<lower=1> n_types;
+	int<lower=1> n_types_each[n_vars];
+	int<lower=1> n_data;
+	int<lower=1> n_events;
+	int<lower=1> n_strategies;
 
-	vector<lower=0>[N_nodal_types] lambdas_prior;
-  int<lower=1> l_starts[N_vars];
-  int<lower=1> l_ends[N_vars];
-  int<lower=1> strategie_starts[N_strategies];
-  int<lower=1> strategie_ends[N_strategies];
+	vector<lower=0>[n_nodal_types] lambdas_prior;
+	int<lower=1> l_starts[n_vars];
+	int<lower=1> l_ends[n_vars];
+  int<lower=1> strategie_starts[n_strategies];
+  int<lower=1> strategie_ends[n_strategies];
 
-  vector[N_nodal_types]  P[N_types];
-  vector[N_nodal_types] inverted_P[N_types] ;
-  matrix<lower=0,upper=1>[N_data, N_types] A;
-  matrix<lower=0,upper=1>[N_events,N_data] A_w;
-  int<lower=0,upper=2147483647> Y[N_events]; // incorrect dimensionality, only for the tryout
+  vector[n_types] P[n_nodal_types] ;
+  vector[n_types] inverted_P[n_nodal_types] ;
+  matrix<lower=0,upper=1>[n_data, n_types] A;
+  matrix<lower=0,upper=1>[n_events,n_data] A_w;
+  int<lower=0,upper=2147483647> Y[n_events]; // incorrect dimensionality, only for the tryout
 
 }
 
 parameters {
-	vector<lower=0>[N_nodal_types - N_vars] gamma;
+	vector<lower=0>[n_nodal_types - n_vars] gamma;
 }
 
 transformed parameters {
-	vector<lower=0>[N_nodal_types] lambdas;
-	vector<lower=1>[ N_vars] sum_gammas;
-	for (i in 1:N_vars) {
-	sum_gammas[i] = 1 + sum(gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]);
-  lambdas[l_starts[i]:l_ends[i]] = append_row(1, gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]) / sum_gammas[i];
+	vector<lower=0>[n_nodal_types] lambdas;
+	vector<lower=1>[ n_vars] sum_gammas;
+	for (i in 1:n_vars) {
+
+	sum_gammas[i] =
+			1 + sum(gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]);
+
+  lambdas[l_starts[i]:l_ends[i]] =
+  		append_row(1, gamma[(l_starts[i] - (i-1)):(l_ends[i] - i)]) / sum_gammas[i];
+
 	}
 }
 
 model {
-	vector[N_data] w;
-	vector[N_events] w_full;
-	vector[N_types] prob_of_types;
-  vector[N_types] P_lambdas[N_nodal_types];
+	vector[n_data] w;
+	vector[n_events] w_full;
+	vector[n_types] prob_of_types;
+  vector[n_nodal_types] P_lambdas[n_types];
 
-
-
- for (j in 1:N_nodal_types) {
-  P_lambdas[j] = P[j] .* lambdas + inverted_P[j];
-
-}
-
- prob_of_types = rows_dotself(  P_lambdas);
-
-
-
-
-
-	print("P_lambdas",  P_lambdas);
+  for (i in 1:n_types) {
+  	for (j in 1:n_nodal_types) {
+  		P_lambdas[i, j] = P[j, i] .* lambdas[j] + inverted_P[j, i];
+  	}
+  	prob_of_types[i] = prod(P_lambdas[i]);
+  }
 
   w = A * prob_of_types;
+  w_full = A_w * w;
 
-	w_full = A_w * w;
 
+  target += gamma_lpdf(lambdas  | lambdas_prior, 1);
+  for (i in 1:n_vars) {
+  	target += -n_types_each[i] * log(sum_gammas[i]);
+  }
 
-	target += gamma_lpdf(lambdas  | lambdas_prior, 1);
-
-	for (i in 1:N_vars) {
-		target += -N_types_each[i] * log(sum_gammas[i]);
-		target += multinomial_lpmf(Y[strategie_starts[i]:strategie_ends[i]] | w_full[strategie_starts[i]:strategie_ends[i]]);
-	}
+  for (i in 1:n_strategies) {
+  	target += multinomial_lpmf(
+  		Y[strategie_starts[i]:strategie_ends[i]] | w_full[strategie_starts[i]:strategie_ends[i]]);
+  }
 }
