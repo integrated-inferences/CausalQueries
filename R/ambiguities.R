46f5bbe708d45b224c19ed4bf2420ea487b25784
macartan <macartan@gmail.com>
2019-04-29 10:23:54 +0200

    new R file

--- R/ambiguities.R
+++ R/ambiguities.R
@@ -218,31 +218,3 @@ reveal_outcomes <- function(model, dos = NULL){
 
 }
 
-#' Get parameter matrix
-#'
-#' Generate a  matrix that maps from parameters into causal types. In models without confounding parameters correspond to nodal types.
-#'
-#' @param model A model created by make_model()
-#'
-#' @export
-#'
-get_parameter_matrix  <- function(model){
-
-	possible_data   <-	get_possible_data(model)
-	nodes           <- names(possible_data)
-	nodal_types     <- get_nodal_types(model)
-	types           <- expand.grid(nodal_types, stringsAsFactors = FALSE)
-	row_identifiers <- unlist(nodal_types)
-
-	# Which nodal_types correspond to a type
-	P <- sapply(1:nrow(types), function(i){
-		type <- types[i,]
-		sapply(row_identifiers, function(nodal_type)
-			all(nodal_type %in% type) )})*1
-
-	colnames(P)  <- do.call(paste, c(types, sep =""))
-	rownames(P ) <- row_identifiers
-	P
-}
-
-
