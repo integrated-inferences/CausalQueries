1aab5eceaf8f930d3a49779332b4424aace3630f
Clara Bicalho <cbm343@nyu.edu>
2019-05-23 20:32:30 +0200

    WIP Add `expand_wildcard()`
    `expand_wildcard()` takes string expression containing wildcard and returns expanded expression maintaining logical relationships.
    
    Example:
    ```
    restriction <- "(M[I=1] < M[I=0]) &
      (D[M=1, I=., P=.] < D[M=0, I=., P=.]) &
      (D[P=1, I=., M=.] < D[P=0, I=., M=.]) &
      (D[I=1, M=., P=.] > D[I=0, M=., P=.])"
    
    expand_wildcard <- expand_restriction(restriction)
    ```
    >Generated expanded restriction:
    ((M[I=1] < M[I=0]))&
    ((D[M=1, I=0, P=0] < D[M=0, I=0, P=0])|
    (D[M=1, I=1, P=0] < D[M=0, I=1, P=0])|
    (D[M=1, I=0, P=1] < D[M=0, I=0, P=1])|
    (D[M=1, I=1, P=1] < D[M=0, I=1, P=1]))&
    ((D[P=1, I=0, M=0] < D[P=0, I=0, M=0])|
    (D[P=1, I=1, M=0] < D[P=0, I=1, M=0])|
    (D[P=1, I=0, M=1] < D[P=0, I=0, M=1])|
    (D[P=1, I=1, M=1] < D[P=0, I=1, M=1]))&
    ((D[I=1, M=0, P=0] > D[I=0, M=0, P=0])|
    (D[I=1, M=1, P=0] > D[I=0, M=1, P=0])|
    (D[I=1, M=0, P=1] > D[I=0, M=0, P=1])|
    (D[I=1, M=1, P=1] > D[I=0, M=1, P=1]))

--- R/helpers.R
+++ R/helpers.R
@@ -34,3 +34,80 @@ st_within <- function(x, left = "[[:punct:]]|\\b", right = "\\["){
 
 	sapply(1:length(starts), function(i) substr(x, starts[i], stops[i]-1))
 }
+
+
+get_str_diff <- function(a, b){
+	as <- strsplit(a, "")[[1]]
+	bs <- strsplit(b, "")[[1]]
+	setdiff(bs[!bs %in% as], ".")
+}
+
+
+expand_wildcard <- function(restriction, joiner = "|"){
+	rest_oper <- sapply(strsplit(restriction, "\\\n"), trimws)
+	rest <- sapply(strsplit(restriction, "\\\n|\\||&"), trimws)
+	relational <- c(">",">=", "<","<=", "==", "!=")
+
+	rest <- setdiff(rest, "")
+	to_expand <- grepl("\\.", rest)
+	# take_operator <- grep("", value = TRUE)
+
+	expanded_types <- sapply(1:length(rest), function(i){
+		if(!to_expand[i])
+			return(rest[i])
+		else {
+			exp_types <- strsplit(rest[i], ".", fixed = TRUE)[[1]]
+			n_types <- (length(exp_types) - 1)/2
+			grid <- replicate(n_types, expr(c(0,1)))
+			type_values <- do.call(expand.grid, grid)
+			is_relational <- sum(sapply(relational, function(i)
+				any(grepl(i, exp_types, fixed = TRUE))))
+			if(is_relational > 0){
+				type_values_v <- t(apply(type_values, 1, rep, times = is_relational+1))
+			}else{
+				type_values_v <- type_values
+			}
+			to_collapse <- cbind(type_values_v, "")
+			ret <- sapply(1:nrow(to_collapse), function(t){
+				tc <- cbind(exp_types, to_collapse[t,])
+				paste0(apply(tc, 1, paste0), collapse = "")
+			})
+
+			return(ret)
+		}
+	})
+
+	operators <- lapply(1:length(expanded_types), function(i){
+		retrieve_operator(a = expanded_types[[i]][1],
+											b = rest_oper[i])
+	})
+
+	these <- lapply(expanded_types, function(a){
+		paste0(a, collapse = paste0(joiner, "\n"))
+	})
+
+	to_print <- paste0(unlist(lapply(these, function(l) paste0("(", l, ")"))),
+										 collapse = paste0(operators, "\n"))
+
+	# to_print <- paste0(unlist(expanded_types), collapse = paste0(joiner, "\n"))
+	cat(paste0("Generated expanded restriction:\n", to_print))
+	to_return <- paste0(unlist(lapply(expanded_types, function(a){
+		paste0("(", a, ")", collapse = joiner)
+	})), collapse = unlist(operators))
+	to_return
+}
+
+
+restriction <- "(M[I=1] < M[I=0]) |
+  (D[M=1, I=., P=.] < D[M=0, I=., P=.]) |
+  (D[P=1, I=., M=.] < D[P=0, I=., M=.]) |
+  (D[I=1, M=., P=.] > D[I=0, M=., P=.])"
+
+expand_wildcard <- expand_restriction(restriction, joiner = "|")
+
+restriction <- "(M[I=1] < M[I=0]) &
+  (D[M=1, I=., P=.] < D[M=0, I=., P=.]) &
+  (D[P=1, I=., M=.] < D[P=0, I=., M=.]) &
+  (D[I=1, M=., P=.] > D[I=0, M=., P=.])"
+
+expand_wildcard <- expand_restriction(restriction)
