f3c0975b483ff8ddaa58220293f31503f64afd8c
macartan <macartan@gmail.com>
2019-05-31 16:28:55 +0200

    more lambdas gone

--- R/gbiqq_designer.R
+++ R/gbiqq_designer.R
@@ -8,7 +8,7 @@
 #' @examples
 #' my_design <- gbiqq_designer(
 #' model <- make_model("X -> Y")
-#'   lambda = c(.5, .5, .1, .7, .1, .1),  # Truth
+#'   parameters = c(.5, .5, .1, .7, .1, .1),  # Truth
 #'   data_strat = list(n = 4, vars = list(NULL), probs = list(NULL), ms = NULL, subsets = list(NULL)),
 #'   queries = list(ATE = "Y[X=1] - Y[X=0]"))
 #'   draw_data(my_design)
@@ -19,7 +19,7 @@ gbiqq_designer <- function(
 	model = make_model("X" %->% "Y"),
 	restrictions = NULL,
 	priors = "uniform",
-	lambda = NULL,        # True parameters
+	parameters = NULL,        # True parameters
 	data_strat = list(n = 1,vars = list(NULL), probs = list(NULL), ms = NULL, subsets = list(NULL)),
 	queries = list(ATE = "Y[X=1] - Y[X=0]")
 ) {
@@ -30,27 +30,27 @@ gbiqq_designer <- function(
 			set_parameter_matrix() %>%
 	   	set_prior_distribution()
 
-	if(is.null(lambda)) {message("No true lambda provided; lambda drawn from prior on each run")}
+	if(is.null(parameters)) {message("No true parameters provided; parameters drawn from prior on each run")}
 
 	data_step <- declare_population(data =
 									data_strategy(
 						    				model,
-						    				lambda = lambda,
+						    				parameters = parameters,
 										    n = data_strat$n,
 										    vars = data_strat$vars,
 										    probs = data_strat$probs,
 										    ms = data_strat$ms,
 										    subsets = data_strat$subsets))
 
-	# Estimand given lambda
+	# Estimand given parameters
 	estimand <- declare_estimand(handler = function(data) {
-		value <- calculate_multiple_estimands(model, lambda = lambda, queries = queries)
+		value <- get_estimands(model, parameters = parameters, queries = queries)
 		data.frame(estimand_label = names(queries), estimand = value[1,])})
 
 	# Estimator runs gbiqq
 	 estimate <- declare_estimator(handler = function(data) {
 		updated <- gbiqq(model = model,  data = data)
-		value <- calculate_multiple_estimands(updated, posterior = TRUE, queries = queries)
+		value <- get_estimands(updated, posterior = TRUE, queries = queries)
 		data.frame(estimate_label = paste0("est_", names(queries)),
 							 estimand = names(queries),
 							 estimate = value[1,],
