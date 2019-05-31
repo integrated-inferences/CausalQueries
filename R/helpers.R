dd10b83b8801929ac5271068761b8402486b52fc
clabima <cbm343@nyu.edu>
2019-05-31 20:00:09 +0200

    Improve `perm()` helper

--- R/helpers.R
+++ R/helpers.R
@@ -2,20 +2,25 @@
 #' Produces the possible permutations of a set of variables
 #'
 #' @param v A vector of integers indicating the number of values each variable takes on. E.g., a binary variable is represented by 2.
-#'
 #' @export
 #'
 #' @return A matrix of permutations
-#'
+#' @importFrom rlang expr
 #' @examples
 #'
 #' \dontrun{
-#' perm(c(2,2,2))
+#' perm(3)
 #' }
-perm <- function(v) {
-	sapply(1:length(v), function(x) {
-		rep(rep(1:v[x], each = prod(v[x:length(v)])/v[x]), length.out = prod(v))
-	}) - 1
+# perm <- function(v) {
+# 	sapply(1:length(v), function(x) {
+# 		rep(rep(1:v[x], each = prod(v[x:length(v)])/v[x]), length.out = prod(v))
+# 	}) - 1
+# }
+perm <- function(n = 2){
+	grid <- replicate(n, expr(c(0,1)))
+	perm <- do.call(expand.grid, grid)
+	colnames(perm) <- NULL
+	perm
 }
 
 #' Get string between two regular expression patterns
@@ -26,7 +31,7 @@ perm <- function(v) {
 #' @param left A regular expression to serve as look ahead.
 #' @param right A regular expression to serve as a look behind.
 #' @return A character vector.
- #' @export
+#' @export
 #' @examples
 #' a <- "(XX[Y=0] == 1) > (XX[Y=1] == 0)"
 #' st_within(a)
