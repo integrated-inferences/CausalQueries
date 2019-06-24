ac0ac7c37855ec004e9a53d3553cdfe41b35ca18
Lily Medina <lilymiru@gmail.com>
2019-06-24 15:14:47 +0200

    gbiqq not rstanarm2

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,5 @@
-# Part of the rstanarm package for estimating model parameters
-# Copyright (C) 2015, 2016, 2017 Trustees of Columbia University
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Code taken from rstanarm
 
 .onLoad <- function(libname, pkgname) { # nocov start
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
@@ -21,8 +7,8 @@
 } # nocov end
 
 .onAttach <- function(...) {
-  gbiqqLib <- dirname(system.file(package = "rstanarm"))
-  pkgdesc <- suppressWarnings(utils::packageDescription("rstanarm", lib.loc = gbiqqLib))
+  gbiqqLib <- dirname(system.file(package = "gbiqq"))
+  pkgdesc <- suppressWarnings(utils::packageDescription("gbiqq", lib.loc = gbiqqLib))
   if (length(pkgdesc) > 1) {
     builddate <- gsub(';.*$', '', pkgdesc$Packaged)
     packageStartupMessage(paste("gbiqq (Version ", pkgdesc$Version, ", packaged: ", builddate, ")", sep = ""))
