b7403b80b7438425b73421365ca3480f4b8b766c
Lily Medina <>
2019-06-27 20:55:08 +0200

    Revert "Merge pull request #50 from macartan/stan_installation_2"
    This reverts commit 22389f6026b86ec8dfe6c93c16d2a8a9bdf1e725, reversing
    changes made to c237aaf269e78f7e47ddff2b0372037835c263e1.

--- R/gbiqq.R
+++ R/gbiqq.R
@@ -1,28 +1,24 @@
+
 #' Fit stan model
 #'
 #' Takes a model and data and returns a model object with data attached and a posterior model
 #' @param model A model as created by \code{make_model}
 #' @param data A data frame with observations
-#' @param ... Arguments passed to \code{rstan::sampling} (e.g. iter, chains).
-#' @importFrom rstan sampling
-#' @import rstantools
+#' @importFrom rstan stan
 #' @importFrom Rcpp cpp_object_initializer
 #' @export
 #'
 #'
-# gbiqq <- function(model, data, ...) {
-# 	stan_data    <- make_gbiqq_data(model = model, data = data)
-# 	# stan_file <- system.file("src/stan_files" ,"simplexes.stan", package = "gbiqq")
-# 	model$posterior_distribution <-	rstan::stan(stanmodels$simplexes, data = stan_data, ...)
-# 	# model$posterior_distribution <-	rstan::stan(file = stan_file, data = stan_data, ...)
-# 	model$data <- data
-# 	model
-# 	}
+gbiqq <- function(model, data,   ...) {
 
-gbiqq <- function(model, data, ...){
-	stan_data <- make_gbiqq_data(model = model, data = data)
-	model$posterior_distribution <- rstan::sampling(object =
-																										stanmodels$simplexes, data = stan_data, ...)
+	stan_file <- system.file("tools" ,"simplexes.stan", package = "gbiqq")
+
+	stan_data    <- make_gbiqq_data(model = model, data = data)
+
+	model$posterior_distribution <-	rstan::stan(file = stan_file, data = stan_data,  ...)
+
 	model$data <- data
+
 	model
-}
+
+	}
