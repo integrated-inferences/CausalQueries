0cef81baf313a32d82906170d23333e8a75e2b1b
Lily Medina <>
2019-07-11 20:33:37 +0200

    integrate diagnosis workflow into diagnose_strategy

--- R/diagnose_strategies.R
+++ R/diagnose_strategies.R
@@ -74,8 +74,10 @@ make_possible_data <- function(model,
 																		 condition = condition[[1]],
 																		 vars = vars[[1]] )
 
-	if(length(N) == 1) return(g_df)
-
+   if(length(N) == 1){
+   	attr(g_df, "possible_data_args") <- list(N = N,within = within, condition = condition, vars = vars)
+   	return(g_df)
+   }
 
 
   for (i in 2:length(N)){
@@ -127,8 +129,11 @@ make_possible_data <- function(model,
   #given <- Reduce(function(x, y) merge(x, y,  by = c("event", "strategy"), all = TRUE), 	out2, right = TRUE)
   #given <- given[,!duplicated(t(given))]
 	#given
-	g_df
 
+
+
+
+  return(g_df)
 }
 
 
@@ -410,8 +415,7 @@ make_data_probabilities <- function(model, pars,  possible_data) {
 make_estimates_database <- function(model,
 																		given,
 																		possible_data = NULL,
-																		queries = "Y[X=1]>Y[X=0]",
-																		...) {
+																		queries = "Y[X=1]>Y[X=0]") {
 
 	if(!exists("fit")) fit  <- fitted_model()
 	if(is.null(possible_data)) possible_data <- make_possible_data(model, given, ...)
@@ -488,17 +492,29 @@ diagnose_strategy <- function(reference_model,
 															queries,
 															estimates_database = NULL,
 															possible_data = NULL,
+															possible_data_args = list(N = list(1),
+																												within = FALSE,
+																												condition = list(TRUE),
+																												vars = list(NULL)),
 															sims = 1000) {
 	# Houskeeping
 
-	if(is.null(possible_data)) possible_data <-
-			make_possible_data(model = reference_model, given = given, N = 1)
+	if(is.null(possible_data)){
+		possible_data_args$model <- reference_model
+		possible_data_args$given <- given
+		possible_data            <- do.call(make_possible_data, possible_data_args)
+
+	} else{
+		possible_data_args       <- attr(possible_data, "possible_data_args")
+
+  }
 
 	if(is.null(estimates_database)) {
 		estimates_database <- make_estimates_database(analysis_model,
 																									given = given,
 																									possible_data = possible_data,
-																									queries = queries)}
+																									queries = queries)
+		}
 
 	# The magic: Draw multiple parameters and get mean squared error over possible data observations as well as posterior variance.
 
@@ -528,5 +544,81 @@ diagnose_strategy <- function(reference_model,
 			MSE      = squared_error%*%probs,
 			post_var = post_var%*%probs)
 		})
-	data.frame(t(as.matrix(c(apply(out, 1, mean), sims = sims), nrow = 1)))
+
+	if(length(possible_data_args$N)==1){
+
+		vars_arg   <- ifelse(is.null(possible_data_args$vars[[1]]), "ALL", possible_data_args$vars[[1]])
+		condition  <- ifelse(is.logical(possible_data_args$condition[[1]]), "-", possible_data_args$condition[[1]])
+		diagnosis           <- data.frame(query   = queries,
+																			condition = condition,
+																			variables = vars_arg,
+																			N         = possible_data_args$N[[1]],
+																			steps     =  1,
+
+																			t(as.matrix(c(apply(out, 1, mean), sims = sims), nrow = 1)),
+																			stringsAsFactors = FALSE)
+
+	} else{
+		diagnosis           <- data.frame(t(as.matrix(c(apply(out, 1, mean), sims = sims), nrow = 1)),
+																			stringsAsFactors = FALSE)
 	}
+
+	return_list <- list()
+	return_list$possible_data       <- possible_data
+	return_list$estimates_database  <- estimates_database
+  return_list$replicates          <- out
+	return_list$diagnosis <- diagnosis
+	class(return_list) <- "strategy_diagnosis"
+	return(return_list)
+}
+
+###
+
+
+
+#' @export
+print.strategy_diagnosis <- function(x, ...) {
+	print(summary(x))
+	invisible(x)
+}
+
+
+#' @export
+summary.strategy_diagnosis <- function(object, ...) {
+	structure(object, class = c("summary.strategy_diagnosis", "data.frame"))
+
+}
+
+#' @export
+print.summary.strategy_diagnosis <- function(x, ...){
+
+	print(x$diagnosis)
+
+}
+
+
+
+#' @export
+print.data_strategy <- function(x, ...) {
+	print(summary(x))
+	invisible(x)
+}
+
+
+#' @export
+summary.data_strategy <- function(object, ...) {
+	structure(object, class = c("summary.data_strategy", "data.frame"))
+
+}
+
+#' @export
+print.summary.data_strategy <- function(x, ...){
+
+	print(x$possible_data)
+
+}
+
+
+
+
+
