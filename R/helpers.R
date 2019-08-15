8afa67f150e645c7a402977eb17375f2f73a98ba
Lily Medina <lilymiru@gmail.com>
2019-08-15 15:12:17 +0200

    redifine restrictions

--- R/helpers.R
+++ R/helpers.R
@@ -39,7 +39,7 @@ perm <- function(max = rep(1, 2)){
 #' b <- "(XXX[[Y=0]] == 1 + XXX[[Y=1]] == 0)"
 #' st_within(b)
 
-st_within <- function(x, left = "[[:punct:]]|\\b", right = "\\[", rm_left = 0, rm_right=-1){
+st_within <- function(x, left = "[^_[:^punct:]]|\\b", right = "\\[", rm_left = 0, rm_right=-1){
 	if(!is.character(x)) stop("`x` must be a string.")
 	puncts <- gregexpr(left, x, perl = TRUE)[[1]]
 	stops <- gregexpr(right, x, perl = TRUE)[[1]]
@@ -193,12 +193,13 @@ interpret_type <- function(model, condition = NULL, position = NULL){
 #'
 #' @param to_expand A character vector of length 1L.
 #' @param join_by A character vector of length 1L.
+#' @param verbose Logical. Whether to print expanded query on the consule.
 #' @importFrom rlang expr
 #' @export
 #' @examples
 #' expand_wildcard("(Y[X=1, M=.] > Y[X=1, M=.])")
 #'
-expand_wildcard <- function(to_expand, join_by = "|"){
+expand_wildcard <- function(to_expand, join_by = "|", verbose = TRUE){
 	orig <- st_within(to_expand, left= "\\(", right="\\)", rm_left = 1)
 	if(is.list(orig)){
 		if(is.null(orig[[1]]))
@@ -245,8 +246,10 @@ expand_wildcard <- function(to_expand, join_by = "|"){
 		oper_return <- apply(oper, 1, function(i) gsub_many(skeleton,
 																												paste0("%expand%", 1:length(orig)),i))
 	}
+	if(verbose){
 	cat("Generated expanded expression:\n")
 	cat(unlist(oper_return), sep = "\n")
+	}
 	oper_return
 }
 
