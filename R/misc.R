c7db31e0c3a06c0f86d45fe3cc70341d7b6db9e1
MerlinHeidmanns <mnh2123@columbia.edu>
2020-02-06 11:18:29 -0500

    misc files for additional functions; may be added to helpers?

---
+++ R/misc.R
@@ -0,0 +1,44 @@
+#' default_stan_control
+#'
+#' @details Sets controls to default unless otherwise specified.
+
+
+default_stan_control <- function(adapt_delta = NULL, max_treedepth = 15L) {
+    adapt_delta = NULL
+    if (is.null(adapt_delta)){
+        adapt_delta <- 0.95
+    }
+    list(adapt_delta = adapt_delta, max_treedepth = max_treedepth)
+}
+
+
+#' set_sampling_args
+#' From rstanarm (November 1st, 2019)
+#'
+#' Set the sampling arguments
+
+set_sampling_args <- function(object, user_dots = list(), user_adapt_delta = NULL, ...) {
+    user_adapt_delta <- NULL
+    args <- list(object = object, ...)
+    unms <- names(user_dots)
+    for (j in seq_along(user_dots)) {
+        args[[unms[j]]] <- user_dots[[j]]
+    }
+    defaults <- default_stan_control(adapt_delta = user_adapt_delta)
+    if (!"control" %in% unms) {
+        args$control <- defaults
+    }
+    else {
+        if (!is.null(user_adapt_delta)) {
+            args$control$adapt_delta <- user_adapt_delta
+        }
+        else {
+            args$control$adapt_delta <- defaults$adapt_delta
+        }
+        if (is.null(args$control$max_treedepth)) {
+            args$control$max_treedepth <- defaults$max_treedepth
+        }
+    }
+    args$save_warmup <- FALSE
+    return(args)
+}
