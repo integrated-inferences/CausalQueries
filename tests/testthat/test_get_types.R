f362e34751691908b68aa1df35b94dbb66ddcdf5
MerlinHeidmanns <mnh2123@columbia.edu>
2020-02-07 16:09:11 -0500

    tests for get_types

--- tests/testthat/test_get_types.R
+++ tests/testthat/test_get_types.R
@@ -6,5 +6,46 @@ testthat::test_that(
 		model <- make_model("X -> M -> Y; X->Y")
 		query <- c("(Y[X = .]==1)", "(Y[X = 0] == 0)")
 		expect_error(get_types(model, query), "Please specify a query of length 1L.")
+		query <- "(Y[Z = .]==1)"
+		expect_error(get_types(model, query), "Variable Z is not part of the model.")
+		query <- "(Y[Z = . == 1)"
+		expect_error(get_types(model, query), "Either '[' or ']' missing.")
+
+	}
+)
+
+context("Proper output.")
+
+testthat::test_that(
+	desc = "Proper output.",
+	code = {
+		model <- make_model("X -> M -> Y; X->Y")
+		query <- "(Y[X = .]==1)"
+		out <- get_types(model, query)
+		expect_equal(out$query, "(Y[X=0]==1 | Y[X=1]==1)")
+		expect_equal(ncol(out$evaluated_nodes), 5)
+		expect_equal(nrow(out$evaluated_nodes), 128)
+		expect_equal(length(out$type_list), 96)
+		expect_equal(out$manipulated_outcomes, "Y")
+		query <- "(Y == 1)"
+		out <- get_types(model, query)
+		expect_equal(nrow(out$evaluated_nodes), 128)
+	}
+)
+
+context("Summary functions of get_types")
+
+testthat::test_that(
+	desc = "Check the summary functions of get_types",
+	code = {
+		model <- make_model("X -> M -> Y; X->Y")
+		query <- "(Y[X = .]==1)"
+		out <- get_types(model, query)
+		expect_output(print(out))
+		summary_out <- class(summary(out))
+		expect_equal(summary_out[1], "summary.causal_types")
+		expect_equal(summary_out[2], "data.frame")
+		out$types <- as.numeric(out$types)
+		expect_output(print(out))
 	}
 )
