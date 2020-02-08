6b7e1e44cb1cea9f4210ff4a162e1e70483bb168
MerlinHeidmanns <mnh2123@columbia.edu>
2020-02-08 07:37:16 -0500

    Tests for simulate data; identified error that a one can't pass a probability vector to the function because rownames(w) is NULL leading to an unequal row error

--- tests/testthat/test_simulate_data.R
+++ tests/testthat/test_simulate_data.R
@@ -1,7 +1,6 @@
 
 context(desc = "Testing make_data")
 
-
 # Simulate using parameters
 model <- make_model("X -> Y")
 
@@ -24,5 +23,27 @@ testthat::test_that(
 	desc = "Positive integer number of observations.",
 	code = {
 		expect_error(make_data(model, n = 0), "Number of observation has to be an integer bigger than 0.")
+	}
+)
+
+context(desc = "Testing simulate_events.")
+
+testthat::test_that(
+	desc = "User input",
+	code = {
+		softmax <- function(x) return(exp(x)/sum(exp(x)))
+		model <- make_model("X -> Y")
+		expect_silent(simulate_events(model = model, parameters = NULL))
+		w <- c(softmax(gtools::rdirichlet(4, 1)))
+	}
+)
+
+textthat::test_that(
+	desc = "Check output.",
+	code = {
+		model <- make_model("X -> Y")
+		n <- rpois(1, 55) + 1
+		out <- simulate_events(model = model, n = n)
+		expect_equal(sum(out[, 2]), n)
 	}
 )
