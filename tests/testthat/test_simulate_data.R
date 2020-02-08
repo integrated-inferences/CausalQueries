a8048595145350d8f0a46a37aa74a911f5b0f2aa
MerlinHeidmanns <mnh2123@columbia.edu>
2020-02-08 09:23:03 -0500

    simulate_events tests

--- tests/testthat/test_simulate_data.R
+++ tests/testthat/test_simulate_data.R
@@ -31,13 +31,30 @@ context(desc = "Testing simulate_events.")
 testthat::test_that(
 	desc = "User input",
 	code = {
-		softmax <- function(x) return(exp(x)/sum(exp(x)))
 		model <- make_model("X -> Y")
 		expect_silent(simulate_events(model = model, parameters = NULL))
-		w <- c(softmax(gtools::rdirichlet(4, 1)))
+		w <- softmax(gtools::rdirichlet(4, 1))
+		rownames(w) <- c("A", "B", "C", "D")
+		expect_equal(as.character(simulate_events(model = model, w = w)[, 1]), rownames(w))
+		w[, 1] <- c(1, 0, 0, 0)
+		expect_equal(simulate_events(model = model, w = w, n = 10)[1, 2], 10)
+		expect_equal(colnames(simulate_events(model = model, include_strategy = TRUE))[2], "strategy")
 	}
 )
 
+testthat::test_that(
+	desc = "param_type input test.",
+	code = {
+		model <- make_model("X -> Y")
+		expect_output(simulate_events(model = model, param_type = "flat"))
+		expect_output(simulate_events(model = model, param_type = "prior_mean"))
+		error_message <- "Posterior distribution required"
+		expect_error(simulate_events(model = model, param_type = "posterior_mean"), error_message)
+		expect_error(simulate_events(model = model, param_type = "posterior_draw"), error_message)
+		expect_is(simulate_events(model = model, param_type = "define", distribution = "jeffreys"), "data.frame")
+	}
+)
+# ("flat", "prior_mean", "posterior_mean", "prior_draw", "posterior_draw", "define)
 textthat::test_that(
 	desc = "Check output.",
 	code = {
@@ -47,3 +64,24 @@ textthat::test_that(
 		expect_equal(sum(out[, 2]), n)
 	}
 )
+
+testthat::test_that(
+	desc = "Check errors.",
+	code = {
+		softmax <- function(x) return(exp(x)/sum(exp(x)))
+		model <- make_model("X -> Y")
+		w <- matrix(softmax(rnorm(4)), nrow = 4, ncol = 1)
+		expect_error(simulate_events(model = model, w = w), "w has to be a matrix with named rows.")
+		w <- c(w)
+		expect_error(simulate_events(model = model, w = w), "w has to be a matrix.")
+	}
+)
+
+testthat::test_that(
+	desc = "Check warnings.",
+	code = {
+		model <- make_model("X -> Y")
+		expect_warning(simulate_events(model = model, param_type = "define"), "neither distribution nor alphas provided; no change to priors")
+	}
+)
+
