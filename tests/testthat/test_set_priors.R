1440dedbcd456922ab07a76361f5e9f169d8f565
Lily Medina <lilymiru@gmail.com>
2020-02-21 13:39:57 +0100

    small fix

--- tests/testthat/test_set_priors.R
+++ tests/testthat/test_set_priors.R
@@ -4,7 +4,7 @@ testthat::test_that(
 	desc = "Check warnings",
 	code = {
 		model <- make_model("X -> Y")
-		expect_warning(make_priors(model, label = c("X0", "Y1"), alphas = c(1)))
+		expect_warning(make_priors(model, label = c("X0", "Y1"), alphas = 1))
 		expect_warning(make_priors(model, statement = c("Y[M=0] > Y[M=1]"), alphas = c(NA)))
 		model <- make_model("X -> Y")
 		expect_warning(make_priors(model, label = c("X0", "Y1"), distribution = c("jeffreys")))
@@ -30,7 +30,7 @@ testthat::test_that(
 	desc = "Check warnings",
 	code = {
 		model <- make_model("X -> Y")
-		expect_warning(make_priors(model, label = c("X0", "Y1"), alphas = c(1)))
+		expect_warning(make_priors(model, label = c("X0", "Y1"), alphas = 1))
 		expect_warning(make_priors(model, statement = c("Y[M=0] > Y[M=1]"), alphas = c(NA)))
 		model <- make_model("X -> Y")
 		expect_warning(make_priors(model, label = c("X0", "Y1"), distribution = c("jeffreys")))
@@ -43,7 +43,7 @@ testthat::test_that(
 	code = {
 		model <- make_model("X -> M -> Y")
 		expect_error(make_priors(model, statement = "Y[X=1] > Y[X=0]", alphas = 3))
-		expect_error(make_priors(model, statement = c("Y[M=0] > Y[M=1]"), alphas = c(-1)))
+		expect_error(make_priors(model, statement = c("Y[M=0] > Y[M=1]"), alphas = -1))
 		expect_error(make_priors(model, statement = "X == 1", alphas = c(2, 0.5))) # Trying to replace  6  parameters with  2 values
 		expect_error(make_priors(model, distribution = c("certainty", "certainty", "jeffreys", "certainty"), node = c("X", "M", "Y", "Z")))
 		expect_error(make_priors(model, node = c("Z"), distribution = c( "uniform"))) # listed node must be in model
