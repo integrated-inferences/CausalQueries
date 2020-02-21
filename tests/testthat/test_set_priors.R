1eb36c5082d0a89e45d3fb0209c32e17b4e4ca86
Lily Medina <lilymiru@gmail.com>
2020-02-21 16:56:20 +0100

    remove test that doesn't produce warning

--- tests/testthat/test_set_priors.R
+++ tests/testthat/test_set_priors.R
@@ -4,7 +4,7 @@ testthat::test_that(
 	desc = "Check warnings",
 	code = {
 		model <- make_model("X -> Y")
-		#expect_warning(make_priors(model, label = c("X0", "Y1"), alphas = 1))
+		expect_warning(make_priors(model, label = c("X0", "Y1")))
 		expect_warning(make_priors(model, statement = c("Y[M=0] > Y[M=1]"), alphas = c(NA)))
 		model <- make_model("X -> Y")
 		#expect_warning(make_priors(model, label = c("X0", "Y1"), distribution = c("jeffreys")))
