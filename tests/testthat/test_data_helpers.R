726c8f24eb8c8ece255d4e1c226ae0cd5d9eaf9f
Lily Medina <lilymiru@gmail.com>
2020-02-27 07:05:53 +0100

    increase covr 93%

---
+++ tests/testthat/test_data_helpers.R
@@ -0,0 +1,107 @@
+context(desc = "Testing data helperse")
+
+testthat::test_that(
+	desc = "collapse_data works",
+	code = {
+		testthat::test_that(
+			desc = "when variables not in the model",
+			code = {
+				 model <- make_model('X->Y')
+				 long_data <- simulate_data(model, n = 6)
+				 data <- collapse_data(long_data, model)
+				 expect_true(class(data) == "data.frame")
+				 expect_true(unique(data$strategy) == "XY")
+			}
+		)
+	}
+)
+
+
+
+testthat::test_that(
+	desc = "collapse_data works when variables not in the model",
+	code = {
+		model <- make_model('X->Y')
+		data <- simulate_data(model, n = 3)
+		Z <- c(0, 0, 0)
+		data <- cbind(data, Z)
+		nodes <- model$nodes
+		data <- collapse_data(data, model)
+		expect_equal(nrow(data), 4)
+	}
+)
+
+testthat::test_that(
+	desc = "collapse_data works when all NA",
+	code = {
+		model <- make_model('X->Y')
+		X <- c(NA, NA, NA)
+		Y <- c(NA, NA, NA)
+		data <- as.data.frame(cbind(X, Y))
+		data <- collapse_data(data, model)
+		expect_equal(nrow(data), 4)
+	}
+)
+
+testthat::test_that(
+	desc = "collapse_data works when node not in data",
+	code = {
+		model <- make_model('X->Y')
+		data <- data.frame(Y = c(1,0,1,0))
+		data <- collapse_data(data, model)
+	  expect_true(!c("X") %in% data$strategy)
+	}
+)
+
+testthat::test_that(
+	desc = "collapse data inconsistent with model and ignored",
+	code = {
+		model <- make_model('X -> Y') %>% set_restrictions('X[]==1')
+		data <- data.frame(X= 0:1)
+		expect_message(collapse_data(data, model))
+	}
+)
+
+
+testthat::test_that(
+	desc = "collapse_data conditions work",
+	code = {
+		model <- make_model('X -> Y')
+		data <- simulate_data(model, n = 4)%>%
+						collapse_data( model, drop_family = TRUE)
+		expect_true(!"strategy" %in% colnames(data))
+		data <- simulate_data(model, n = 4)%>%
+						collapse_data( model, summary = TRUE)
+		expect_true(class(data) == "list")
+		expect_equal(names(data), c("data_events","observed_events", "unobserved_events"))
+	}
+)
+
+
+testthat::test_that(
+	desc = "expand_data works",
+	code = {
+		model <-  make_model('X -> Y')
+		events <- make_events(model, n = 4)
+		data <- expand_data(events, model)
+		expect_equal(nrow(data), 4)
+		expect_equal(names(data), c("X", "Y"))
+		data <- expand_data(model = model)
+	 }
+)
+
+testthat::test_that(
+	desc = "expand_data errors",
+	code = {
+		model <-  make_model('X -> Y')
+		events <- make_events(model, n = 5)
+		expect_error(expand_data(events, model = 'X -> Y'))
+		expect_error(expand_data(c(events = c("X0YO", "X1Y1")), model))
+		colnames(events) <- NULL
+		expect_error(expand_data(events, model))
+			}
+)
+
+
+
+
