ffc99286e1a8b4f74e14d129db232ea996d66fcb
Julio Saul Solis Arce <solis@ad.wzb.eu>
2020-02-28 15:18:59 +0100

    Update test_make_confounders.R

--- tests/testthat/test_make_confounders.R
+++ tests/testthat/test_make_confounders.R
@@ -1,12 +1,13 @@
 context("Testing make_confounds")
 
-
 testthat::test_that(
 	desc = "Check messages.",
 	code = {
 		model <- make_model("X -> Y")
 		expect_message(make_confounds_df(model))
-		expect_message(model <- set_confound(model, list(X = 'X==1')))
+		expect_warning(model <- model %>%
+									 	set_confound(list(X = 'X==1')))
 	}
 )
+
 
