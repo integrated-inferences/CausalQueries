e760ad193f391cb61ffc22cc997f3d5f51fdbb52
MerlinHeidmanns <mnh2123@columbia.edu>
2020-03-06 10:41:10 -0500

    now 100%

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -4,10 +4,9 @@ testthat::test_that(
 	desc = "sampling_args user_adapt_delta",
 	code = {
 		object <- NULL
-		user_dots <- NULL
-		user_adapt_delta = 10
-		out <- gbiqq:::set_sampling_args(object = object, user_dots = user_dots, user_adapt_delta = user_adapt_delta)
-		expect_equal(out$control$adapt_delta, user_adapt_delta)
+		user_dots = list(control = list(user_adapt_delta = 10))
+		out <- gbiqq:::set_sampling_args(object = object, user_dots = user_dots, user_adapt_delta = 10)
+		expect_equal(out$control$adapt_delta, 10)
 	}
 )
 
