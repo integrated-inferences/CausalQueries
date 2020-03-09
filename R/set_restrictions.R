60428ec2fab0795ef3bd9a4d20853faef263f68b
Julio Saul Solis Arce <solis@ad.wzb.eu>
2020-03-09 10:02:55 +0100

    Revert "Update set_restrictions.R"
    This reverts commit 3badc96bc4a55332cbff77a8cdb3738e1d5067d2.

--- R/set_restrictions.R
+++ R/set_restrictions.R
@@ -183,6 +183,9 @@ restrict_by_query <- function(model, statement, join_by = "|", keep = FALSE) {
 
     n_restrictions <- length(statement)
 
+    if (!is.logical(keep))
+        stop("`keep` should be either 'TRUE' or 'FALSE'")
+
     if (length(join_by) == 1) {
         join_by <- rep(join_by, n_restrictions)
     } else if (length(join_by) != n_restrictions) {
