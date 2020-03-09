12281df15782e86478efc12eb2002f42170f7944
Julio Saul Solis Arce <solis@ad.wzb.eu>
2020-03-09 09:32:39 +0100

    Update test_get_ambiguity_matrix.R

--- tests/testthat/test_get_ambiguity_matrix.R
+++ tests/testthat/test_get_ambiguity_matrix.R
@@ -8,3 +8,18 @@ testthat::test_that(
 		expect_true(!is.null(model$A))
 	}
 )
+
+testthat::test_that(
+	desc = "If the matrix is there, return it",
+	code = {
+		model <- make_model("X -> Y")
+		model <- set_ambiguities_matrix(model)
+		A <- get_ambiguities_matrix(model)
+		expect_is(A, "ambiguities_matrix")
+		expect_equal(ncol(A), 4)
+		expect_equal(nrow(A), 8)
+		model2 <-  make_model("X -> Y")
+		B <- make_ambiguities_matrix(model2)
+		expect_equal(as.vector(A), as.vector(B))
+	}
+)
