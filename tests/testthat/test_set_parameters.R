090c4af3cec137ad97f312a0a0fae797c83366d3
till-tietz <ttietz2014@gmail.com>
2022-04-04 12:12:37 +0200

    fixed test issues arising from old confounding syntax

--- tests/testthat/test_set_parameters.R
+++ tests/testthat/test_set_parameters.R
@@ -55,11 +55,11 @@ testthat::test_that(
 		expect_true(class(set_parameters(model)) == "causal_model")
 
 		out <- make_model('X -> Y') %>%
-  		set_confound(list(X = 'Y[X=1]>Y[X=0]'))  %>%
-  		set_parameters(confound = list(X='Y[X=1]>Y[X=0]', X='Y[X=1]<=Y[X=0]'),
-                 parameters = list(c(.2, .8), c(.8, .2))) %>%
+  		set_confound("X <-> Y")  %>%
+  		set_parameters(confound = list(X='Y[X=1]>Y[X=0]'),
+  		               parameters = c(.2, .8))%>%
   		get_parameters()
-		expect_equal(sum(0.2 == out), 2)
+		expect_equal(sum(0.2 == out), 1)
 		}
 )
 
