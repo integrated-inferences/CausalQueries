fba9dcac1db61432ba9e01c0d4ff9fed30b70c58
Jonah Foong <foong@ad.wzb.eu>
2022-04-05 15:31:00 +0200

    add tests, add labels for plotdag

--- R/plot_dag.R
+++ R/plot_dag.R
@@ -17,10 +17,10 @@
 #' @import ggdag
 #' @import dplyr
 #' @import ggplot2
+#' @import stringr
 #' @importFrom graphics plot
 #' @importFrom dagitty dagitty
 #' @importFrom repr repr_text
-#' @importFrom stringr str_split
 #' @importFrom latex2exp TeX
 #'
 #' @examples
@@ -28,74 +28,109 @@
 #' \dontrun{
 #' model <- CausalQueries:::make_model('X -> K -> Y; X <-> Y')
 #' CausalQueries:::plot_dag(model)
+#'
+#' labels <- c(expression(paste(theta^A)),expression(paste(theta^B)),expression(paste(theta^C)))
+#' CausalQueries:::plot_dag(model,x_coord=c(1,2,3),y_coord=c(3,2,1),lab_names=labels,labels = TRUE)
 #' }
 #'
 
 plot_dag <- function(model = NULL,
                      x_coord = NULL,
                      y_coord = NULL,
+                     lab_names = NULL,
+                     labels = FALSE,
                      title = "",
                      textcol = 'white',
                      textsize = 3.88,
                      shape = 16,
                      nodecol = 'black',
-                     nodesize = 16
+                     nodesize = 16,
+                     labsize = 3.88,
+                     ...
 ) {
-    # Checks
-    if(is.null(model))
-        stop("Model object must be provided")
-    if(class(model)!="causal_model") stop("Model object must be of type causal_model")
-    if (is.null(x_coord) == !is.null(y_coord)) message("Coordinates should be provided for both x and y (or neither).")
-    if (!is.null(x_coord) & !is.null(y_coord) & length(x_coord)!=length(y_coord)) stop("x and y coordinates must be of equal length")
-    if (!is.null(x_coord) & !is.null(y_coord) & length(model$nodes)!=length(x_coord)) stop("length of coordinates supplied must equal number of nodes")
-    if (!is.null(model$P) & is.null(model$confounds_df)) {
-        message("Model has a P matrix but no confounds_df. confounds_df generated on the fly. To avoid this message try model <- set_confounds_df(model)")
-        model <- set_confounds_df(model)
-    }
+  # Checks
+  if (is.null(model))
+    stop("Model object must be provided")
+  if (class(model)!="causal_model") stop("Model object must be of type causal_model")
+  if (is.null(x_coord) & is.null(y_coord)) message("neither x nor y coordinates supplied; coordinates will be randomly generated")
+  if (is.null(x_coord) & !is.null(y_coord)) stop("x coordinates are missing")
+  if (!is.null(x_coord) & is.null(y_coord)) stop("y coordinates are missing")
+  if (!is.null(x_coord) & !is.null(y_coord) & length(x_coord)!=length(y_coord)) stop("x and y coordinates must be of equal length")
+  if (!is.null(x_coord) & !is.null(y_coord) & length(model$nodes)!=length(x_coord)) stop("length of coordinates supplied must equal number of nodes")
+  if (!is.null(lab_names) & length(model$nodes)!=length(lab_names)) stop("length of labels supplied must equal number of nodes")
+  if (!is.null(model$P) & is.null(model$confounds_df)) {
+    message("Model has a P matrix but no confounds_df. confounds_df generated on the fly. To avoid this message try model <- set_confounds_df(model)")
+    model <- set_confounds_df(model)
+  }
 
-    # Get names
-    nodes <- model$nodes
+  # Get names
+  nodes <- if (is.null(lab_names)) model$nodes else LETTERS[1:length(lab_names)]
 
-    # Get statement
+  statement <- ifelse(!is.null(lab_names),
+                      stringr::str_replace_all(model$statement,setNames(nodes,model$nodes)),
+                      model$statement)
 
-    dagitty_statement <-  paste("dag{", model$statement, "}") %>%
-      dagitty::dagitty()
+  # Get statement
 
-
-    # Add coordinates if provided (otherwise generated)
-
-    if(!is.null(x_coord) & !is.null(y_coord)){
-        names(x_coord) <- nodes
-        names(y_coord) <- nodes
-
-        dagitty::coordinates(dagitty_statement) <-
-            list(x = x_coord , y = y_coord) %>%
-            coords2df() %>% coords2list()
-    }
-
-    # Make the df
-    df <- dagitty_statement %>% ggdag::tidy_dagitty()
-    df$data <- df$data %>% mutate(
-        update=paste0(name,to))
+  dagitty_statement <-  paste("dag{", statement, "}") %>%
+    dagitty::dagitty()
 
 
-    # Step 2: Format and export
-    df %>%
-        ggplot(aes(x=x,y=y,xend=xend,yend=yend)) +
-        geom_dag_point(colour=nodecol,shape=shape,
-                       size=nodesize) +theme_dag() +
-        labs(title = latex2exp::TeX(repr::repr_text(title) %>% stringr::str_remove_all('\\"'))) +
-        geom_dag_text(aes_string(label = 'name'),
-                      show.legend = FALSE,
-                      parse = TRUE,
-                      color=textcol,
-                      size=textsize,
-        ) +
-        geom_dag_edges()
+  # Add coordinates if provided (otherwise generated)
+
+  if(!is.null(x_coord) & !is.null(y_coord)){
+    names(x_coord) <- nodes
+    names(y_coord) <- nodes
+
+    dagitty::coordinates(dagitty_statement) <-
+      list(x = x_coord , y = y_coord) %>%
+      ggdag::coords2df() %>% ggdag::coords2list()
+  }
+
+  # Make the df
+  df <- dagitty_statement %>% tidy_dagitty()
+  df$data <- df$data %>% mutate(
+    label = if(is.null(lab_names)) name else
+      lab_names %>% as.character %>% .[match(df$data$name,LETTERS)],
+    end = if(is.null(lab_names)) to else
+      lab_names %>% as.character %>% .[match(df$data$to,LETTERS)],
+    update=paste0(label,end),
+    pos=match(label,lab_names)) %>%
+    arrange(-desc(pos))
+
+
+  # Step 2: Format and export
+  p <- df %>%
+    ggplot(aes(x=x,y=y,xend=xend,yend=yend)) +
+    geom_dag_point(colour=nodecol,shape=shape,
+                   size=nodesize) +theme_dag() +
+    labs(title = TeX(repr_text(title) %>% stringr::str_remove_all('\\"')))
+
+  if (labels==TRUE){
+    p +
+      geom_dag_label_repel(aes_string(label = 'label'),
+                           show.legend = FALSE,
+                           parse = TRUE,
+                           size = labsize,
+                           ...) +
+      geom_dag_edges()
+
+  } else {
+    # Labels centered on nodes
+    p +
+      geom_dag_text_repel(aes_string(label = 'label'),
+                          show.legend = FALSE,
+                          parse = TRUE,
+                          color=textcol,
+                          size=textsize,
+                          box.padding = 0,
+                          force = 0
+      ) + geom_dag_edges()
+  }
 }
 
 #' @export
 plot.causal_model <- function(x, ...) {
-    plot_dag(x,...)
+  plot_dag(x,...)
 }
 
--- tests/testthat/test_plot_dag.R
+++ tests/testthat/test_plot_dag.R
@@ -4,53 +4,68 @@ context(desc = "Testing plot_dag")
 testthat::skip_on_cran()
 testthat::test_that(
 
-  desc = "Testing plot.dag",
-
-  code = {
-    model <- make_model("X -> M -> Y; X -> Y")
-    pdf(file = NULL)
-    expect_silent(plot(model))
-    model <- make_model("X -> M -> Y")  %>%
-      set_confound(confound = list(M = "Y[M=1]==1"))
-    model$confounds_df <- NULL
-    expect_that(plot_dag(model), shows_message())
-  }
-)
-
-testthat::test_that(
-
-  desc = "Testing warning",
-
-  code = {
-    model <- make_model('X -> K -> Y; X -> Y')
-    model <- set_parameter_matrix(model)
-    expect_message(plot_dag(model))
-  }
-)
-
-testthat::test_that(
-
   desc = "Testing coordinates",
 
   code = {
-    model <- make_model('X -> K -> Y')
+    model <- make_model("X -> K -> Y")
     x <- c(1,2,3)
     y <- c(1,1,1)
-    P <- plot_dag(model,x_coord=x,y_coord=y)
+    P <- plot(model,x_coord=x,y_coord=y)
     dat <- data.frame(name=model$nodes,x=x,y=y)
-    expect_false(!any(P$data[match(P$data$name,model$nodes),c('name','x','y')]==dat))
+    expect_false(!any(P$data[match(P$data$name,model$nodes),c("name","x","y")]==dat))
   }
 )
 
 testthat::test_that(
 
-  desc = "Testing obscure",
+  desc = "Testing default coordinates with label names",
 
   code = {
-    model <- make_model('X -> K -> Y')
-    obs <- "X->K"
-    P <- plot_dag(model,obscure=obs)
-    expect_true(P$data %>% dplyr::filter(grepl("X",name) & grepl("K",to)) %>% .$direction %>%
-                  is.na())
+    model <- make_model("X -> K -> Y")
+    names <- paste0("test",1:3)
+    expect_message(plot(model,lab_names=names))
   }
 )
+
+testthat::test_that(
+
+  desc = "Testing setting labels",
+
+  code = {
+    model <- make_model("X -> K -> Y")
+    expect_message(plot(model, labels = TRUE))
+  }
+)
+
+testthat::test_that(
+
+  desc = "Testing additional arguments",
+
+  code = {
+    model <- make_model("X -> K -> Y")
+    P <- plot(model, labels = TRUE,segment.size=20, colour="blue")
+    z <- P$layers[[2]]
+    expect_true(z$aes_params$colour=="blue" & z$aes_params$segment.size==20)
+  }
+)
+
+testthat::test_that(
+
+  desc = "Test stop arguments",
+
+  code = {
+    expect_error(CausalQueries:::plot_dag(model=NULL))
+    expect_error(CausalQueries:::plot_dag(model=c(1,2,3)))
+    expect_error(plot(model,x_coord = c(1,2,3), y_coord = c(2,1)))
+    model <- make_model("X -> K -> Y")
+    expect_error(plot(model,x_coord = c(1,2),y_coord = c(3,2)))
+    expect_error(plot(model, lab_names = paste0("test",1:4)))
+    expect_error(plot(model, x_coord = c(1,2,3)))
+    expect_error(plot(model, y_coord = c(1,2,3)))
+    expect_message(plot(model))
+    model <- make_model("X -> K -> Y; X -> Y")
+    model <- set_parameter_matrix(model)
+    expect_message(plot(model),x_coord=c(1,2,3),y_coord=c(3,2,1))
+  }
+)
+
