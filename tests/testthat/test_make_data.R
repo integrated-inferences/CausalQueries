aae464126ae56b5ab6090a920b75b685ad3067fc
till-tietz <ttietz2014@gmail.com>
2022-06-16 15:49:12 +0200

    fixed tests

--- tests/testthat/test_make_data.R
+++ tests/testthat/test_make_data.R
@@ -12,7 +12,7 @@ testthat::test_that(
 
 	code = {
 		model <- make_model("X -> Y") %>%
-			set_priors(priors = c(1, 1, 1, 0, 0 , 0))
+			set_priors(alphas = c(1, 1, 1, 0, 0 , 0))
 		out <- colSums(make_data_single(model, n = 1e3, param_type = "prior_draw"))
 		expect_true(out[1] > out[2])
 		model <- make_model("X -> Y") %>%
