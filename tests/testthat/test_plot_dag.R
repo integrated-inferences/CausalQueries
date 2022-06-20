bb73ae8f285b0f59dc52dd15bb314a23a63c0c50
macartan <macartan@gmail.com>
2022-06-20 16:15:59 +0200

    Update test_plot_dag.R

--- tests/testthat/test_plot_dag.R
+++ tests/testthat/test_plot_dag.R
@@ -10,22 +10,10 @@ testthat::test_that(
     model <- make_model("X -> M -> Y; X -> Y")
     pdf(file = NULL)
     expect_silent(plot(model))
-    model <- make_model("X -> M -> Y")  %>%
-      set_confound(confound = list("M <-> Y"))
-    expect_that(plot_dag(model), shows_message())
+    dev.off()
   }
 )
 
-testthat::test_that(
-
-  desc = "Testing warning",
-
-  code = {
-    model <- make_model('X -> K -> Y; X -> Y')
-    model <- set_parameter_matrix(model)
-    expect_message(plot_dag(model))
-  }
-)
 
 testthat::test_that(
 
@@ -50,7 +38,7 @@ testthat::test_that(
     model <- make_model("X -> K -> Y")
     x <- c(1,2,3)
     y <- c(1,1,1)
-    P <- plot_dag(model,x_coord=x,y_coord=y)
+    P <- CausalQueries:::plot_dag(model,x_coord=x,y_coord=y)
     dat <- data.frame(name=model$nodes,x=x,y=y)
     expect_false(!any(P$data[match(P$data$name,model$nodes),c('name','x','y')]==dat))
     expect_message(plot(model,x_coord=x))
