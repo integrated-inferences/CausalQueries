0fcd56e0896a6c75ce64a1035438cdb07fdea940
till-tietz <ttietz2014@gmail.com>
2022-09-30 15:06:15 +0200

    optimized realise outcomes

--- R/realise_outcomes.R
+++ R/realise_outcomes.R
@@ -7,6 +7,7 @@
 #' @inheritParams CausalQueries_internal_inherit_params
 #' @param dos A named \code{list}. Do actions defining node values, e.g., \code{list(X = 0, M = 1)}.
 #' @param node A character. An optional quoted name of the node whose outcome should be revealed. If specified all values of parents need to be specified via \code{dos}.
+#' @param add_rownames logical indicating whether to add causal types as rownmaes to the ouput
 #' @return A \code{data.frame} object of revealed data for each node (columns) given causal / nodal type (rows) .
 #' @export
 #' @examples
@@ -20,95 +21,92 @@
 #' model <- make_model("X->M->Y")
 #' realise_outcomes(model, dos = list(M = 1), node = "Y")
 #'}
-realise_outcomes <- function(model, dos = NULL, node = NULL){
+realise_outcomes <- function(model, dos = NULL, node = NULL, add_rownames = FALSE){
 
 	# Housekeeping
-	if(!is.null(node) & is.null(dos)) stop("Do actions must be specified when node is not NULL")
+	if(!is.null(node) & is.null(dos)){
+	  stop("Do actions must be specified when node is not NULL")
+	}
 
-	nodal_types <- get_nodal_types(model, collapse = FALSE)
-	parents_list  <- get_parents(model)
+  nodal_types <- get_nodal_types(model, collapse = FALSE)
+  nodal_types_collapsed <- get_nodal_types(model, collapse = TRUE)
+  parents_list  <- get_parents(model)
 
 	# Case with node specified
 	if(!is.null(node)){
 
 		parents <- parents_list[[node]]
 
-			if(any(!names(dos) %in% parents)) {
-				conjugation <- ifelse (sum(!names(dos) %in% parents)>1, "are not parents of", "is not a parent of")
-				subjects <- paste0(names(dos)[!names(dos) %in% parents], collapse = ", ")
-				if(all(!names(dos) %in% parents))
-					stop(paste(subjects, conjugation, node))
-				else
-					warning(paste(subjects, conjugation, node))
+		if(any(!names(dos) %in% parents)) {
+			conjugation <- ifelse (sum(!names(dos) %in% parents)>1, "are not parents of", "is not a parent of")
+			subjects <- paste0(names(dos)[!names(dos) %in% parents], collapse = ", ")
+			if(all(!names(dos) %in% parents)){
+				stop(paste(subjects, conjugation, node))
+			} else {
+				warning(paste(subjects, conjugation, node))
 			}
-
-			nodal_type_var <- nodal_types[[node]]
-			nodal_label    <- apply(nodal_type_var, 1,paste, collapse ="")
-
-			dos_rep        <- sapply(parents, function(p) rep(dos[p], length(nodal_label)))
-
-			data_realizations <- data.frame(dos_rep, nodal_label, stringsAsFactors = FALSE)
-
-			names(data_realizations) <- c(parents, node)
-			types <- data_realizations
-
-			endogenous_vars   <- 	node
-			types_of_endogenous        <- data.frame(types[, endogenous_vars], stringsAsFactors = FALSE)
-			names(types_of_endogenous) <- endogenous_vars
-			in_dos <- names(dos) # vars in do list
 		}
 
+		nodal_type_var <- nodal_types[[node]]
+		dos_rep <- as.character(sapply(parents, function(p) rep(dos[p], nrow(nodal_type_var))))
+
+		data_realizations <- data.frame(dos_rep, rownames(nodal_type_var), stringsAsFactors = FALSE)
+		names(data_realizations) <- c(parents, node)
+
+		endogenous_vars <- node
+		in_dos <- names(dos)
+
+		if(add_rownames){
+		  types <- data_realizations
+		  types_of_endogenous <- data.frame(types[, endogenous_vars], stringsAsFactors = FALSE)
+		  names(types_of_endogenous) <- endogenous_vars
+		}
+
+	}
+
+	#case without node specified
 	if(is.null(node)) {
 
-		types           <- get_causal_types(model)
-		exogenous_vars  <- attr(model, "root_nodes")
-		endogenous_vars <- attr(model, "nonroot_nodes")
-		types_of_exogenous         <- data.frame(types[, exogenous_vars])
-		names(types_of_exogenous)  <- exogenous_vars
-		types_of_endogenous        <- data.frame(types[, endogenous_vars], stringsAsFactors = FALSE)
-		names(types_of_endogenous) <- endogenous_vars
-		data_realizations <- types
-		in_dos <- names(dos) # vars in do list
+	  data_realizations <- get_causal_types(model)
+	  endogenous_vars <- attr(model, "nonroot_nodes")
+	  in_dos <- names(dos) # vars in do list
+
+	  if(add_rownames){
+	    types <- data_realizations
+	  }
 
 		# Fill in values for dos
-		if(!is.null(dos)) for(j in 1:length(dos)) data_realizations[, in_dos[j]] <- dos[[j]] # [1] mh 20191008 Commented out this "[1]" as it was stopping filling in a vector of dos for nested expression
-
+		if(!is.null(dos)){
+		  for(j in 1:length(dos)){
+		    data_realizations[, in_dos[j]] <- as.character(dos[[j]])
+		  }
 		}
+
+	}
 
 	# Magic: Work though each endogenous node in sequence and substitute its implied values
-	for(j in 1:ncol(types_of_endogenous)) {
-		if(!(endogenous_vars[j] %in% in_dos)){   # skip if do applied to var
+	endogenous_vars <- endogenous_vars[!(endogenous_vars %in% in_dos)]
 
-			var            <- endogenous_vars[j]
-			child_type     <- types_of_endogenous[,j]
-			parents        <- parents_list[[var]]
-			nodal_type_var <- nodal_types[[var]]
-			nodal_label    <- apply(nodal_type_var, 1, paste, collapse ="")
+	data_realizations <- CQBigModel::realise_outcome_c(d = data_realizations,
+	                                                   endogenous_nodes = endogenous_vars,
+	                                                   parents_list = parents_list,
+	                                                   nodal_types_collapsed = nodal_types_collapsed,
+	                                                   nodal_types = nodal_types)
 
-			J <- sapply(1:length(child_type), function(i){
-				type        <- child_type[i]
-				parents_val <- data_realizations[i, parents]
-				parents_val <- trimws(paste(parents_val, collapse = ""))
-				row         <- which(nodal_label == type )
-				outcome     <- nodal_type_var[row, parents_val]
-				outcome
-			})
-			data_realizations[, endogenous_vars[j]] <- J
-		}
+	# add rownames
+	if(add_rownames){
+	  if(is.null(node)){
+	    rownames(data_realizations) <- apply(types, 1, paste, collapse = ".")
+	    type_names <- matrix(sapply(1:ncol(types), function(j) paste0(names(types)[j], types[,j])), ncol = ncol(types))
+	    attr(data_realizations, "type_names") <- apply(type_names, 1, paste,  collapse = ".")
+	  } else {
+	    attr(data_realizations, "type_names") <-
+	      rownames(data_realizations) <- apply(types_of_endogenous, 1, FUN = function(x) paste0(x))
+	  }
 	}
 
-	# Prep for export
-	if(is.null(node)){
-		rownames(data_realizations) <- apply(types, 1, paste, collapse = ".")
-		type_names <- matrix(sapply(1:ncol(types), function(j) paste0(names(types)[j], types[,j])), ncol = ncol(types))
-		attr(data_realizations, "type_names") <- apply(type_names, 1, paste,  collapse = ".")
-	} else {
-		attr(data_realizations, "type_names") <-
-			rownames(data_realizations) <- apply(types_of_endogenous, 1, FUN = function(x)paste0(# node,  ## Not uncomment to include node name
-				x))
-	}
 
-	data_realizations
+	return(data_realizations)
 
 }
 
