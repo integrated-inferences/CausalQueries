010be4f6d40db82ef211574e2a91e114991c4fbb
gerasy1987 <gs2716@columbia.edu>
2023-12-12 15:14:17 -0600

    correct test error

--- tests/testthat/test_get_type_prob.R
+++ tests/testthat/test_get_type_prob.R
@@ -6,6 +6,7 @@
 context("Testing get_type_prob")
 
 testthat::skip_on_cran()
+
 testthat::test_that(
   desc = "get_type_prob",
   code = {
@@ -22,7 +23,7 @@ testthat::test_that(
 		expect_identical(get_type_prob(model),
 		                 get_type_prob(model, parameters = rep(1, 6)))
 
-		model <- model |> set_confound(list('X <-> Y'))
+		model <- set_confound(model, list('X <-> Y'))
 		expect_identical(get_type_prob(model),
 		                 get_type_prob(model, parameters = rep(1, 10)))
 
@@ -31,10 +32,75 @@ testthat::test_that(
 
 testthat::test_that(
 
+  desc = "get_type_prob_multiple",
+  code = {
+    expect_equal(
+      dim(suppressMessages(get_type_prob_multiple(make_model('X->Y'),
+                                                  using = 'priors',
+                                                  n_draws = 3))),
+      c(8,3)
+    )
+
+    expect_message(
+      get_type_prob_multiple(make_model('X->Y'),
+                             using = 'priors',
+                             n_draws = 3),
+      "Prior distribution added to model"
+    )
+
+    expect_equal(
+      length(get_type_prob_multiple(make_model('X->Y'),
+                             using = 'parameters')),
+      8
+    )
+
+  }
+)
+
+testthat::test_that(
+
 	desc = "get_param_dist",
 	code = {
-		expect_error(get_param_dist(model = make_model('X->Y'),
-		                            using = 'posteriors', n_draws = 4))
+		expect_error(
+		  get_param_dist(model = make_model('X->Y'),
+		                 using = 'posteriors',
+		                 n_draws = 4),
+		  "Model does not contain a posterior distribution"
+		)
+
+	  expect_equal(
+	    make_model("X -> Y") |>
+	      update_model(data.frame(X = 0, Y = 0),
+	                   refresh = 0, chains = 1, iter = 10, warmup = 5) |>
+	      suppressWarnings() |>
+	      get_param_dist(model = _,
+	                     using = 'posteriors',
+	                     n_draws = 4) |>
+	      dim(),
+	    c(5, 6)
+	  )
+
+	  expect_type(
+	    get_param_dist(model = make_model('X->Y'),
+	                   using = 'parameters',
+	                   n_draws = 4),
+	    "double"
+	  )
+
+	  expect_message(
+	    get_param_dist(model = make_model('X->Y'),
+	                   using = 'priors',
+	                   n_draws = 4),
+	    "Prior distribution added to model"
+	  )
+
+	  expect_type(
+	    suppressMessages(
+	      get_param_dist(model = make_model('X->Y'),
+	                     using = 'priors',
+	                     n_draws = 4)),
+	    "list"
+	  )
 	}
 )
 
