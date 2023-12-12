e669414c57c30bb93babf8e6f6504caf0f01f751
gerasy1987 <gs2716@columbia.edu>
2023-12-12 16:29:18 -0600

    update tests for plot_dag

--- tests/testthat/test_plot_dag.R
+++ tests/testthat/test_plot_dag.R
@@ -33,31 +33,47 @@ testthat::test_that(
     model <- make_model("X -> K -> Y")
     x <- c(1, 2, 3)
     y <- c(1, 1, 1)
-    P <-
-      CausalQueries:::plot_dag(model,
-                               x_coord = x,
-                               y_coord = y)
+    P <- CausalQueries:::plot_dag(model,
+                                  x_coord = x,
+                                  y_coord = y)
+
     dat <- data.frame(name = model$nodes,
                       x = x,
                       y = y)
-    expect_false(!any(P$data[match(P$data$name, model$nodes),
-                             c('name', 'x', 'y')] == dat))
-    expect_message(plot(model, x_coord = x))
-    expect_message(plot(model, y_coord = y))
+    expect_false(
+      !any(P$data[match(P$data$name, model$nodes),
+                  c('name', 'x', 'y')] == dat)
+    )
+
+    expect_message(
+      CausalQueries:::plot_dag(model, x_coord = x)
+    )
+
+    expect_message(
+      CausalQueries:::plot_dag(model, y_coord = y)
+    )
 
   })
 
 testthat::test_that(
-  desc = "Test stop arguments",
+  desc = "Error messages work",
   code = {
-    expect_error(CausalQueries:::plot_dag(model = NULL))
-    expect_error(CausalQueries:::plot_dag(model = c(1, 2, 3)))
-    expect_error(plot(model,
-                      x_coord = c(1, 2, 3),
-                      y_coord = c(2, 1)))
     model <- make_model("X -> K -> Y")
-  })
 
+    expect_error(CausalQueries:::plot_dag(model = NULL),
+                 "Model object must be provided")
+    expect_error(CausalQueries:::plot_dag(model = c(1, 2, 3)),
+                 "Model object must be of type causal_model")
+    expect_error(CausalQueries:::plot_dag(model,
+                      x_coord = c(1, 2, 3),
+                      y_coord = c(2, 1)),
+                 "x and y coordinates must be of equal length")
+    expect_error(CausalQueries:::plot_dag(model,
+                                          x_coord = c(1, 2),
+                                          y_coord = c(2, 1)),
+                 "length of coordinates supplied must equal number of nodes")
+
+  })
 
 
 
