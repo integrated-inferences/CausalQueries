b8d6887fda75242b81effb1c1b32ab1628211dff
till-tietz <ttietz2014@gmail.com>
2024-12-02 19:52:11 +0100

    dependency fixes

--- R/CausalQueries-package.R
+++ R/CausalQueries-package.R
@@ -4,7 +4,7 @@
 #' update over models given data, and calculate arbitrary causal queries.
 #' Model definition makes use of dagitty type syntax.
 #' Updating is implemented in 'stan'.
-#' @docType _PACKAGE
+#' @docType package
 #'
 #' @name CausalQueries
 #' @importFrom utils globalVariables
@@ -15,5 +15,5 @@ globalVariables(names = c("posterior", "prob", "restrict", "n", "model", "data",
                           "%>%", "node", "nodal_type","param_set", "event",
                           "count", "strategy",  "distinct", "e", "v", "w",
                           "gen","children","given","param_value","priors",
-                          "g"
+                          "g", "label", "query"
     ))
